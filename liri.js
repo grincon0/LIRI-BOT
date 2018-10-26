37f5ee2205cccb1a70c8f2127fc4787113349736
Wake <georgerincon2@gmail.com>
2018-10-26 03:31:39 -0400

    added instrc demos

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const fs = require('fs');
 const moment = require('moment');
 
-//imports API key directed from key.js and
+//imports API key directed from key.js
 let key = require("./keys.js");
 
 //get arguments that will be used by the app to recieve data through the APIs
@@ -15,40 +15,34 @@ let userInput1 = process.argv[2];
 let userInput2 = process.argv[3];
 
 
-const log = (data) => {
-    
-        fs.appendFile("log.txt", `\r\n\r\n${data}`, function(err){
-            if(err){
-                console.log(err);
-            }else{
-                console.log('Log has been updated!');
-            }
-            
-        });
-        
-}
 
 const doFromTxt = () => {
-    fs.readFile("random.txt", "utf8", function(error, data){
+    fs.readFile("random.txt", "utf8", function (error, data) {
         getSongInfo(data);
     })
 }
 
 const getSongInfo = (name) => {
     let spotify = new Spotify(key.spotify);
+
+    if (!name) {
+        name = "The Sign";
+    }
     spotify.search({ type: 'track', query: name }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
         console.log(`Song Name : ${name.toUpperCase()}\nAlbum : ${data.tracks.items[0].album.name}\nArtist : ${data.tracks.items[0].album.artists[0].name}\nURL : ${data.tracks.items[0].album.external_urls.spotify}`);
+
+
     });
 }
 
 
 const getMovieInfo = (name) => {
 
-    if(!name){
+    if (!name) {
         name = "Mr.Nobody";
     }
     axios.get(`http://www.omdbapi.com/?t=${name}&plot=short&apikey=trilogy`)
@@ -63,11 +57,11 @@ const getMovieInfo = (name) => {
 const getConcertInfo = (name) => {
     axios.get(`https://rest.bandsintown.com/artists/${name}/events?app_id=codingbootcamp`)
         .then(function (response) {
-            if(!response.data.length){
+            if (!response.data.length) {
                 console.log(`No upcoming concerts for ${name}`);
-            }else{
+            } else {
                 let source = response.data;
-                for(let i =0; i < source.length; i++){
+                for (let i = 0; i < source.length; i++) {
                     console.log(`\nLocation : ${source[i].venue.city},${source[i].venue.country}\nVenue : ${source[i].venue.name}\nDate : ${moment(source[i].datetime).format("MM/DD/YYYY")} \n`);
                 }
             }
